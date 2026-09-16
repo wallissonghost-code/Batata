234700b099456e8a4ca4788f27c35834fd2a523b
ghostzinho <wallissonghost@gmail.com>
2026-09-15 22:05:22 -0300

    refactor: centralize game asset paths

---
+++ js/config/assets.js
@@ -0,0 +1,20 @@
+export const ASSET_PATHS = Object.freeze({
+  arena: 'FB798BF0-2248-4F8C-9F80-D88DDD6B05C0.png',
+  doll: Object.freeze({
+    back: 'assets/doll/back.png',
+    turn01: 'assets/doll/turn-01.png',
+    turn02: 'assets/doll/turn-02.png',
+    front: 'assets/doll/front.png'
+  }),
+  guards: Object.freeze({
+    circle: 'assets/guards/circle.png',
+    triangle: 'assets/guards/triangle.png',
+    square: 'assets/guards/square.png'
+  }),
+  players: Object.freeze({
+    p1: 'assets/players/p1/',
+    p2: 'assets/players/p2/',
+    p3: 'assets/players/p3/',
+    p4: 'assets/players/p4/'
+  })
+});
